448c0448a9f3ef5876a4a3f43273e598e9802126
alfredzxc <100396962+alfredzxc@users.noreply.github.com>
2022-11-28 17:34:10 +0800

    Update page1.js

--- page1.js
+++ page1.js
@@ -1,8 +1,25 @@
+const generator = document.querySelector(".generator")
+userinput = generator.querySelector(".form input"),
+button = generator.querySelector(".form button"),
+img = generator.querySelector(".QR-Code img");
 
+button.addEventListener("click", () => {
+    let textbox = userinput.value;
+    if(!textbox) return;
+    button.innerText = "Processing info..."
+    img.src = 'https://api.qrserver.com/v1/create-qr-code/?size=130x130&data=/qrValue';
+    img.addEventListener("load", () => {
+        generator.classList.add("active");
+        button.innerText = "Submit"
+    });
+});
 
+userinput.addEventListener("keyup", () => {
 
-
-
+    if(!userinput.value){
+        generator.classList.remove("active");
+    }
+});
 
 
 
