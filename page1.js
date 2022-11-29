36844f5d968a65af5fb2c1c7b820d88045be660b
alfredzxc <100396962+alfredzxc@users.noreply.github.com>
2022-11-29 21:29:13 +0800

    Update page1.js

--- page1.js
+++ page1.js
@@ -7,11 +7,7 @@ button.addEventListener("click", () => {
     let textbox = userinput.value;
     if(!textbox) return;
     button.innerText = "Processing info..."
-<<<<<<< Updated upstream
-    img.src = 'https://api.qrserver.com/v1/create-qr-code/?size=130x130&data=walang_api_kaya_isang_qrcode_lang_nalabas_HAHAHHAHA';
-=======
     img.src = 'https://api.qrserver.com/v1/create-qr-code/?size=130x130&data=pakitanggal ng PR2 sa subjects pls';
->>>>>>> Stashed changes
     img.addEventListener("load", () => {
         generator.classList.add("active");
         button.innerText = "Submit"
